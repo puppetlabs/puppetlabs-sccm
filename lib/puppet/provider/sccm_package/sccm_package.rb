23c75713ac2ac239fd3cfde9135a6dc99673239f
kreeuwijk <kevin.reeuwijk@puppet.com>
2021-03-31 02:31:06 +0200

    add failed auth message

--- lib/puppet/provider/sccm_package/sccm_package.rb
+++ lib/puppet/provider/sccm_package/sccm_package.rb
@@ -128,6 +128,7 @@ class Puppet::Provider::SccmPackage::SccmPackage < Puppet::ResourceApi::SimplePr
   def recursive_download_list(uri, auth_type = 'none', auth_user = nil, auth_domain = nil, auth_password = nil)
     result = {}
     head = make_request(uri, :head, auth_type, auth_user, auth_domain, auth_password)
+    raise Puppet::ResourceError, "Failed to connect to SCCM Distribution Point! Got error #{head.code}, #{head.message}" unless head.code.to_i == 200
     if head['Content-Length'].to_i.positive? && head['Content-Type'] == 'application/octet-stream'
       result[uri] = head['Content-Length']
     elsif head['Content-Length'].to_i.zero?
